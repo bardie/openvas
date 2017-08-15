7e3a41d5a39c6193d46c86f1b700940db790467a
bardie <blawdws@gmail.com>
2017-08-15 19:34:18 +0300

    Create openvas.sh

---
+++ openvas.sh
@@ -0,0 +1,18 @@
+while read -r line ; do
+
+id="$(echo $line | cut -d ' ' -f 1)"
+status="$(echo $line | cut -d ' ' -f 2)"
+
+#echo "$id"
+#echo "$status"
+
+if [ "$status" == "Done" ]; then
+
+omp -u USER -w PASSWORD -R "$id" > /var/log/openvas/reports/"$id".xml
+
+else
+
+echo "Scan has not completed"
+fi
+
+done < <(omp -u USER -w PASSWORD --get-tasks --details | grep -E '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z')
