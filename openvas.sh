3796fc5ac29e766c624ce8a1a49db320ed2c6d20
bardie <blawdws@gmail.com>
2018-02-06 18:33:41 +0300

    Update openvas.sh

--- openvas.sh
+++ openvas.sh
@@ -7,12 +7,11 @@ status="$(echo $line | cut -d ' ' -f 2)"
 #echo "$status"
 
 if [ "$status" == "Done" ]; then
-
+if [ ! -f /var/log/openvas/reports/"$id".xml ]; then
 omp -u USER -w PASSWORD -R "$id" > /var/log/openvas/reports/"$id".xml
 
 else
 
 echo "Scan has not completed"
 fi
-
 done < <(omp -u USER -w PASSWORD --get-tasks --details | grep -E '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z')
